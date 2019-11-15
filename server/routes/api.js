acf9658333eda09c005459e94f566ecc7a864a12
Alex Colombo <uricaine@hotmail.com>
2019-11-15 13:08:49 +0100

    new method to post users

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
+const User = require('../models/user');
+
 const mongoose = require('mongoose');
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
+
 const db = 'mongodb://localhost/users';
 
 mongoose.connect(db, err => {
@@ -18,6 +21,19 @@ mongoose.connect(db, err => {
 
 router.get('/', (req, res) => {
   res.send('From API route');
+});
+
+router.post('/register', (req, res) => {
+  let userData = req.body;
+  console.log(userData);
+  let usu = new User(userData);
+  usu.save((err, registeredUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.status(200).send(registeredUser);
+    }
+  });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,15 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const PORT = 3000;
-const api = require('./routes/api');
 const app = express();
-
 app.use(bodyParser.json());
+const api = require('./routes/api');
 
-app.use('/api', api);
 app.get('/', (req, res) => {
   res.send('hello from server');
 });
+app.use('/api', api);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
