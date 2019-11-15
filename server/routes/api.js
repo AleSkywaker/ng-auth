117317a47c565eb923f63b5c4f8c3f94d13fae67
Alex Colombo <uricaine@hotmail.com>
2019-11-15 12:12:44 +0100

    connectd to mongo

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const router = express.Router();
 
+const mongoose = require('mongoose');
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+const db = 'mongodb://localhost/users';
+
+mongoose.connect(db, err => {
+  if (err) {
+    console.log('Error' + err);
+  } else {
+    console.log('conectado a mongoDB');
+  }
+});
+
 router.get('/', (req, res) => {
   res.send('From API route');
 });
