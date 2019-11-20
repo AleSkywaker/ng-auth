e13f3dbd2e7b77435bb8e6026ee92aee569c5e55
Alex Colombo <uricaine@hotmail.com>
2019-11-20 10:06:05 +0100

    import HttpClientModule

--- ngApp/src/app/app.module.ts
+++ ngApp/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './register/register.component';
@@ -17,7 +18,7 @@ import { LibrosProComponent } from './libros-pro/libros-pro.component';
     LibrosComponent,
     LibrosProComponent
   ],
-  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
+  imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent]
 })
