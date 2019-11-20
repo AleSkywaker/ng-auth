5b3c5cd16bbbd8c1f8651b175d69c31b8fc60e63
Alex Colombo <uricaine@hotmail.com>
2019-11-20 11:22:25 +0100

    use cors

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const PORT = 3000;
 const app = express();
-// app.use(bodyParser.json());
+
+//middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+//cors
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method'
+//   );
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+
+//   next();
+// });
 const api = require('./routes/api');
 
 app.get('/', (req, res) => {
