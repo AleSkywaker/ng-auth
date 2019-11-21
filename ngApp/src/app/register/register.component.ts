6cadcc305b2e9a3cb2f3e0549946dc5e3a19c534
Alex Colombo <uricaine@hotmail.com>
2019-11-21 19:42:49 +0100

    type the request

--- ngApp/src/app/register/register.component.ts
+++ ngApp/src/app/register/register.component.ts
@@ -2,11 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from '../auth.service';
 
-interface User {
-  email: string;
-  password: string;
-}
-
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
