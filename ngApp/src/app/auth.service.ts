e018f5a14c96660b9747c2afe0f317ff11691be8
Alex Colombo <uricaine@hotmail.com>
2019-11-24 12:19:07 +0100

    (feature) new route and method to login

--- ngApp/src/app/auth.service.ts
+++ ngApp/src/app/auth.service.ts
@@ -7,9 +7,14 @@ import { User } from './models/user';
 })
 export class AuthService {
   private registerUrl = 'http://localhost:3000/api/register';
+  private loginUrl = 'http://localhost:3000/api/login';
   constructor(private http: HttpClient) {}
 
   registerUser(user: User) {
     return this.http.post<any>(this.registerUrl, user);
+  }
+
+  loginUser(user: User) {
+    return this.http.post<any>(this.loginUrl, user);
   }
 }
