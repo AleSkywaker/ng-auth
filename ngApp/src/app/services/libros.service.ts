9bd5cb29fda915c664c115f5fdf25c4720975a0a
Alex Colombo <uricaine@hotmail.com>
2019-11-27 16:59:08 +0100

    implementing navigation

--- ngApp/src/app/services/libros.service.ts
+++ ngApp/src/app/services/libros.service.ts
@@ -1,17 +1,20 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class LibrosService {
-  private librosUrl = 'http://localhost:3000/api/libros';
-  private librosProUrl = 'http://localhost:3000/api/librosPro';
+  private librosUrl = "http://localhost:3000/api/libros";
+  private librosProUrl = "http://localhost:3000/api/librosPro";
   constructor(private http: HttpClient) {}
   getLibros() {
     return this.http.get<any>(this.librosUrl);
   }
   getLibrosPro() {
     return this.http.get<any>(this.librosProUrl);
+  }
+  loggedIn(): boolean {
+    return !!localStorage.getItem("token");
   }
 }
